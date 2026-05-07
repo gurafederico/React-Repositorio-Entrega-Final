a0fadb5d28561945480a557d94be4c37db78cdb9
Gura Federico <gurafederico@gmail.com>
2026-05-07 16:48:45 -0300

    cosas

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react"; // Importamos el hook
 import { Count } from "../Count/Count";
 import "./ItemDetail.css"; 
 import { useCart } from "../Context/CartContext";
 
 export const ItemDetail = ({ item }) => {
   const { addToCart } = useCart();
-  
+  const [agregado, setAgregado] = useState(false); 
+
   const onAdd = (quantity) => {
-    // agrego el producto al carrito
     addToCart(item, quantity);
+    
+    
+    setAgregado(true);
+
+    
+    setTimeout(() => {
+      setAgregado(false);
+    }, 2500);
+
     console.log(`Agregado al carrito: ${item.name} - cantidad: ${quantity}`);
   };
 
@@ -23,7 +33,6 @@ export const ItemDetail = ({ item }) => {
     <div className="detail-master-container">
       <div className="detail-box">
         
-        
         <div className="info-side">
           <h2 className="team-title">{item.name}</h2>
           <p className="description-text">
@@ -31,7 +40,6 @@ export const ItemDetail = ({ item }) => {
           </p>
         </div>
 
-        
         <div className="card-side">
           <img 
             src={item.image} 
@@ -42,12 +50,18 @@ export const ItemDetail = ({ item }) => {
           <div className="mini-content">
             <span className="mini-price">US$ {item.price}</span>
             
-            
             <Count 
               stock={item.stock || 10} 
               initial={1} 
               onAdd={onAdd} 
             />
+
+            {/* cartel si se hace click en el boton dea agregar */}
+            {agregado && (
+              <div className="confirmacion-agregado">
+                <p>🏈 ¡Sumaste el producto al carrito! 🏈</p>
+              </div>
+            )}
           </div>
         </div>
 
