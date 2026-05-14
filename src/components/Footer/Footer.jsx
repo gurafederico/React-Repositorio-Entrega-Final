e7fc86e8e4a1b195d484a1ec878a85d5f51a6f4c
Gura Federico <gurafederico@gmail.com>
2026-05-14 18:27:39 -0300

    cambios footer, agregados iconos whatapp e instagram

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,22 @@
 import "./Footer.css";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa"; 
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="footer-container">
       <p>© 2026 NFL Goods Store Argentina &trade; |</p>
       <nav>
         <ul className="nav-list">
+          <li>
+            <a href="https://instagram.com/tu_usuario" target="_blank" rel="noopener noreferrer">
+              <FaInstagram className="social-icon" /> Instagram
+            </a>
+          </li>
+          <li>
+            <a href="https://wa.me/tu_numero_de_telefono" target="_blank" rel="noopener noreferrer">
+              <FaWhatsapp className="social-icon" /> WhatsApp
+            </a>
+          </li>
         </ul>
       </nav>
     </footer>
