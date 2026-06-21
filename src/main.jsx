aed5ec9a14d14b97c9f712b48bfd6029d9921226
Gura Federico <gurafederico@gmail.com>
2026-06-21 18:51:51 -0300

    cambios en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
-import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./Context/CartContext";
+import { AuthProvider } from "./Context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
